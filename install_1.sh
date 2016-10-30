fbefc0c2a19ec2f0df761480087978dd1689a0b8
Damian Poddebniak <duesee@users.noreply.github.com>
2016-10-30 21:12:33 +0100

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -56,7 +56,7 @@ mount /dev/sda2 /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 
-pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr vim zsh git
+pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr vim zsh git dialog
 
 genfstab -pU /mnt >> /mnt/etc/fstab
 
